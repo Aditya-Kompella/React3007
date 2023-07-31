c82c609b15757f0c42e2e64ace12b8a9c7508513
aditya96kc <aditya@collekto.in>
2023-07-31 17:29:52 +0530

    initial changes

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 /**
- * 
- * HMR - Hot Module Reloading
+ * parcel created a server
+ * HMR - Hot Module Replacement
  * File Watcher algorithm written in c++
- * bundling 
+ * bundling
  * minifying
  * cleaning our code
  * dev and production builds
  * Super fast build algorithm
  * Image optimization
  * caching while development
- * 
- * 
+ * compression
+ * compatible with older versions of browsers
+ * it adds polyfills for older browsers
+ * gives  enability to work on https on dev
+ * manages port number
+ * we should put parcel.cache in our gitignore folder anything that we can generate on our server should kept in .gitignore
+ * uses consistent hasing algorithm for caching
+ * zero config bundler
+ * transitive dependencies -- we have our package manager which handles the dependency tree and take care of transitive dependencies
+ * browser list is a thing which is used to determine which browsers we support
+ * tree shaking
+ * means that we don't need to bundle all the dependencies
+ * parcel uses the tree shaking algorithm and we don't need the bundles it removes itself
+ *
+ *
  */
 import React from "react";
 import ReactDOM from "react-dom"; // Corrected import statement
